8d6e4efac63d8fe0c81aa58d61cb73865a8b40a8
Sahil Sood <sahil28sood@gmail.com>
2022-05-13 10:26:22 -0400

    few more

--- app/src/main/java/com/example/foody/RecipesFragment.kt
+++ app/src/main/java/com/example/foody/RecipesFragment.kt
@@ -1,19 +1,22 @@
 package com.example.foody
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.foody.databinding.FragmentRecipesBinding
 
 class RecipesFragment : Fragment() {
-
+    lateinit var binding: FragmentRecipesBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipes, container, false)
+        binding = FragmentRecipesBinding.inflate(inflater, container, false)
+        binding.recyclerView.showShimmer()
+        return binding.root
     }
 
 }
